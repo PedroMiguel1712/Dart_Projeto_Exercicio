9610877c8a941b7420df748ed96da81e7c65c1f3
Pedro Miguel <159182184+PedroMiguel1712@users.noreply.github.com>
2025-02-21 22:11:24 -0300

    Add files via upload

---
+++ Exc04Dart.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  int a = 2;
+  int b = 3;
+  int c = 4;
+  double areaT = (a * b)/2;
+  double areaC = (c * c) * 3.14;
+  double areaTR = ((a + b) * c)/2;
+  int areaQ = b * b;
+  int areaR = a * b;
+  print("A area do triangulo é: $areaT");
+  print("A area do circulo é: $areaC");
+  print("A area do trapezio é: $areaTR");
+  print("A area do quadrado é: $areaQ");
+  print("A area do retangulo é: $areaR");
+}
---
+++ Exc05Dart.dart
@@ -0,0 +1,65 @@
+// No país de Cotemigos a moeda nacional é a merreca (M$). Sabe-se que sistema
+// monetário de Cotemigos só utiliza moedas, não utiliza cédulas, e que os valores dos
+// diferentes tipos de moeda são os seguintes:
+// Valor (M$) Descrição
+// 1,00 | Moeda de uma merreca
+// 5,00 | Moeda de cinco merrecas
+// 10,00 | Moeda de dez merrecas
+// 50,00 | Moeda de cinqüenta merrecas
+// 100,00 | Moeda de cem merrecas
+
+// Devido ao acúmulo de moedas por parte dos Cotemigos, o Governo resolveu abrir uma
+// concorrência internacional para o desenvolvimento de um software, escrito em linguagem C.
+// Dado um valor em merreca, o programa deve calcular qual o número mínimo de moedas
+// necessárias para perfazer o valor especificado.
+// Exemplo: Se o valor for M$ 187,00, a saída de programa deve ser:
+// 1 moeda(s) de M$ 100,00
+// 1 moeda(s) de M$ 50,00
+// 3 moeda(s) de M$ 10,00
+// 1 moeda(s) de M$ 5,00
+// 2 moeda(s) de M$ 1,00
+
+void main()
+{
+  
+  int merreca = 187;
+  int num;
+  if (merreca%100 != 0)
+  {
+    num = (merreca/100).toInt();
+    print(num.toString()+ " moeda(s) de M 100,00");
+    merreca = merreca - (num * 100);
+  }
+  if (merreca%50 != 0)
+  {
+    num = (merreca/50).toInt();
+    print(num.toString()+ " moeda(s) de M 50,00");
+    merreca = merreca - (num * 50);
+  }
+   if (merreca%10 != 0)
+  {
+    num = (merreca/10).toInt();
+    print(num.toString()+ " moeda(s) de M 10,00");
+    merreca = merreca - (num * 10);
+  }
+  if (merreca%5 != 0)
+  {
+    num = (merreca/5).toInt();
+    print(num.toString()+ " moeda(s) de M 5,00");
+    merreca = merreca - (num * 5);
+  }
+  if (merreca%5 != 0)
+  {
+    num = (merreca/1).toInt();
+    print(num.toString()+ " moeda(s) de M 1,00");
+    merreca = merreca - (num * 1);
+  }
+  
+  
+}
+
+
+
+
+
+
